3a69eb89df6a469c1f3102de69ac5197ab13fe40
moonaim ghouali <ia_ghouali@esi.dz>
2023-02-06 13:04:35 +0100

    initial setup

---
+++ src/data/sideBarData.js
@@ -0,0 +1,21 @@
+export const links = [
+    {
+      name: 'calendar',
+      link : "/",
+      icon: "",
+    },
+    {
+      name: 'kanban',
+      link : "/",
+      icon: "",
+    },
+    {
+      name: 'editor',
+      link : "/",
+      icon: "",
+    },
+    {
+      name: 'color-picker',
+      link : "/",
+      icon: "",
+    }]
